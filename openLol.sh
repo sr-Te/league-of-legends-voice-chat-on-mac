41d9396e451a6a3db501a3fabac72265b9b84bd9
sr-Te <jorge.eff.96@gmail.com>
2022-01-08 22:18:44 -0300

    script done

---
+++ openLol.sh
@@ -0,0 +1,6 @@
+# Goes into the app's contents
+cd '/Applications/League of Legends.app/Contents/LoL/'
+# Runs the client
+./LeagueClient.app/Contents/MacOS/LeagueClient
+# Kills the script monitor that makes de the cog icon from the Automator task show
+killAll ScriptMonitor
